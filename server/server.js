217b3384906c12cb65076e52fcd9255fef016d82
lyes sadmi <lyes.sadmi@epitech.eu>
2018-02-01 23:16:19 +0100

    delete /todo/:id

--- server/server.js
+++ server/server.js
@@ -8,52 +8,69 @@ var {User} = require('./model/users');
 
 
 var app = express();
-
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     console.log(req.body);
-    var todo   = new Todo({
+    var todo = new Todo({
         text: req.body.text
     });
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
-app.get('/todos',(req,res)=>{
-   Todo.find().then((todos)=>{
-       res.send({todos});
-   },(e)=>{
-       res.status(400).send(e);
-   })
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
 });
 
-app.get('/todos/:id', (req,res)=>{
+app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
-        if(todo){
-            res.send({todo} );
-        }else{
-           return res.status(404).send();
+        if (todo) {
+            res.send({todo});
+        } else {
+            return res.status(404).send();
         }
-    }).catch((e)=>{
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
         res.status(400).send(e);
     })
 })
 
 
-app.listen(3000, () =>{
-    console.log('Start on port 3000');
+app.listen(port, () => {
+    console.log(`Start on port ${port}`);
 })
 
 
 module.exports = {
-      app
+    app
 }
